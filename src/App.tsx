74e5ad5e52f0009b53baf0fd8c3ec8c229f0b03e
Norbert Táskai <taskain@gmail.com>
2024-02-28 06:15:33 +0100

    2.22

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,32 @@ import Todo from "./Todo";
 function App() {
 
   const [todos, setTodos] = useState(() => [new Todo('Boltba menni'), new Todo('Kutyát etetni')]);
+  const [toggleConfirmation, setToggleConfirmation] = useState(false);
+  const [selectedTodoToDelete, setSelectedTodoToDelete] = useState<Todo>();
 
-  function onRemove(todo: Todo) {
+  function handleRemove(todo: Todo) {
+    setToggleConfirmation(true);
+    setSelectedTodoToDelete(todo);
+  }
+
+  function confirmDelete() {
+    const todo = selectedTodoToDelete;
     const updatedTodos = todos.filter(it => it !== todo);
     setTodos(updatedTodos);
+    setSelectedTodoToDelete(undefined);
+    setToggleConfirmation(false);
   }
 
   return (
     <div className="App">
       <h1>Todos</h1>
-      {todos.map((todo, i) => (<TodoItem key={i} todo={todo} onRemove={() => onRemove(todo)}/>))}
+      {todos.map((todo, i) => (<TodoItem key={i} todo={todo} onRemove={() => handleRemove(todo)}/>))}
+      {toggleConfirmation &&
+          <div>
+            <p>Confirmation</p>
+            <button onClick={confirmDelete}>Confirm</button>
+          </div>
+      }
     </div>
   );
 }
